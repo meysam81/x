30a6898aac14b16c8c76ef70dc81e902d12112e4
Meysam <meysam@developer-friendly.blog>
2025-05-23 00:03:42 +0700

    feat: add koanf config

---
+++ config/lib.go
@@ -0,0 +1,110 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	"github.com/knadh/koanf/parsers/json"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+)
+
+type options struct {
+	delimiter        string
+	jsonConfig       string
+	jsonWatchEnabled bool
+	yamlConfig       string
+	yamlWatchEnabled bool
+}
+
+func WithDelimiter(delim string) func(*options) {
+	return func(o *options) {
+		o.delimiter = delim
+	}
+}
+
+func WithJsonConfig(filename string) func(*options) {
+	return func(o *options) {
+		o.jsonConfig = filename
+	}
+}
+
+func WithYamlConfig(filename string) func(*options) {
+	return func(o *options) {
+		o.yamlConfig = filename
+	}
+}
+
+func WithoutJsonWatch() func(*options) {
+	return func(o *options) {
+		o.jsonWatchEnabled = false
+	}
+}
+
+func WithoutYamlWatch() func(*options) {
+	return func(o *options) {
+		o.yamlWatchEnabled = false
+	}
+}
+
+func NewConfig(opts ...func(*options)) (k *koanf.Koanf, err error) {
+	o := &options{
+		delimiter:        ".",
+		jsonWatchEnabled: true,
+		yamlWatchEnabled: true,
+	}
+
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	k = koanf.New(o.delimiter)
+
+	err = loadConfig(k, o)
+	if err != nil {
+		return nil, err
+	}
+
+	return k, nil
+}
+
+func loadConfig(k *koanf.Koanf, o *options) (err error) {
+	if o.jsonConfig != "" {
+		_, err := os.Stat(o.jsonConfig)
+		if err != nil {
+			return err
+		}
+
+		f := file.Provider(o.jsonConfig)
+
+		// TODO: provide watch functionality
+
+		err = k.Load(f, json.Parser())
+		if err != nil {
+			return err
+		}
+	}
+
+	if o.yamlConfig != "" {
+		_, err := os.Stat(o.yamlConfig)
+		if err != nil {
+			return err
+		}
+
+		err = k.Load(file.Provider(o.yamlConfig), yaml.Parser())
+		if err != nil {
+			return err
+		}
+	}
+
+	err = k.Load(env.Provider("", "_", func(s string) string {
+		return strings.ToLower(strings.ReplaceAll(s, "_", "."))
+	}), nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,18 +3,29 @@ module github.com/meysam81/x
 go 1.24.2
 
 require (
+	github.com/knadh/koanf/parsers/json v1.0.0
+	github.com/knadh/koanf/parsers/yaml v1.0.0
+	github.com/knadh/koanf/providers/env v1.1.0
+	github.com/knadh/koanf/providers/file v1.2.0
+	github.com/knadh/koanf/v2 v2.2.0
 	github.com/mattn/go-sqlite3 v1.14.28
 	modernc.org/sqlite v1.37.1
 )
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/knadh/koanf/maps v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/libc v1.65.7 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
