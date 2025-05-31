14dd775e360c85d39c589be7eaa38796e2154207
Meysam Azad <meysam@developer-friendly.blog>
2025-05-31 15:07:53 +0700

    feat(config): allow providing defaults in constructor
    it eases the usage with only one api call

--- config/config.go
+++ config/config.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/knadh/koanf/parsers/json"
 	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/confmap"
 	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
@@ -18,13 +19,11 @@ type options struct {
 	yamlConfig       string
 	yamlWatchEnabled bool
 	envPrefix        string
+	defaultValues    map[string]interface{}
+	defaultProvided  bool
 }
 
 type Config = koanf.Koanf
-type unloadedConfig struct {
-	k *Config
-	o *options
-}
 
 func WithDelimiter(delim string) func(*options) {
 	return func(o *options) {
@@ -62,11 +61,19 @@ func WithEnvPrefix(p string) func(*options) {
 	}
 }
 
-func NewConfig(opts ...func(*options)) (*unloadedConfig, error) {
+func WithDefaults(defaults map[string]interface{}) func(*options) {
+	return func(o *options) {
+		o.defaultValues = defaults
+		o.defaultProvided = true
+	}
+}
+
+func NewConfig(opts ...func(*options)) (*Config, error) {
 	o := &options{
 		delimiter:        ".",
 		jsonWatchEnabled: true,
 		yamlWatchEnabled: true,
+		defaultProvided:  false,
 	}
 
 	for _, opt := range opts {
@@ -75,15 +82,43 @@ func NewConfig(opts ...func(*options)) (*unloadedConfig, error) {
 
 	k := koanf.New(o.delimiter)
 
-	return &unloadedConfig{
-		k,
-		o,
-	}, nil
-}
+	if o.defaultProvided {
+		err := k.Load(confmap.Provider(o.defaultValues, o.delimiter), nil)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-func (u *unloadedConfig) Build() (err error) {
-	err = u.k.Load(env.ProviderWithValue(u.o.envPrefix, ".", func(key, value string) (string, interface{}) {
-		k := strings.TrimPrefix(key, u.o.envPrefix)
+	if o.jsonConfig != "" {
+		_, err := os.Stat(o.jsonConfig)
+		if err != nil {
+			return nil, err
+		}
+
+		f := file.Provider(o.jsonConfig)
+
+		// TODO: provide watch functionality
+
+		err = k.Load(f, json.Parser())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if o.yamlConfig != "" {
+		_, err := os.Stat(o.yamlConfig)
+		if err != nil {
+			return nil, err
+		}
+
+		err = k.Load(file.Provider(o.yamlConfig), yaml.Parser())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err := k.Load(env.ProviderWithValue(o.envPrefix, ".", func(key, value string) (string, interface{}) {
+		k := strings.TrimPrefix(key, o.envPrefix)
 		k = strings.ToLower(k)
 		k = strings.ReplaceAll(k, "__", "-") // BASE__URL => base-url
 		k = strings.ReplaceAll(k, "_", ".")  // SERVE_PORT => serve.port
@@ -96,40 +131,8 @@ func (u *unloadedConfig) Build() (err error) {
 	}), nil)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if u.o.jsonConfig != "" {
-		_, err := os.Stat(u.o.jsonConfig)
-		if err != nil {
-			return err
-		}
-
-		f := file.Provider(u.o.jsonConfig)
-
-		// TODO: provide watch functionality
-
-		err = u.k.Load(f, json.Parser())
-		if err != nil {
-			return err
-		}
-	}
-
-	if u.o.yamlConfig != "" {
-		_, err := os.Stat(u.o.yamlConfig)
-		if err != nil {
-			return err
-		}
-
-		err = u.k.Load(file.Provider(u.o.yamlConfig), yaml.Parser())
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (u *unloadedConfig) Koanf() *Config {
-	return u.k
+	return k, nil
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.24.2
 require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/knadh/koanf/parsers/json v1.0.0
 	github.com/knadh/koanf/parsers/yaml v1.0.0
+	github.com/knadh/koanf/providers/confmap v1.0.0
 	github.com/knadh/koanf/providers/env v1.1.0
 	github.com/knadh/koanf/providers/file v1.2.0
 	github.com/knadh/koanf/v2 v2.2.0
