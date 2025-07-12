fbf3b805c5ec63c02291187777cafaa98e1e0487
Meysam Azad <meysam@developer-friendly.blog>
2025-07-12 15:36:46 +0700

    feat: add OTEL tracing lib

--- go.mod
+++ go.mod
@@ -10,18 +10,23 @@ require (
 	github.com/knadh/koanf/providers/confmap v1.0.0
 	github.com/knadh/koanf/providers/env v1.1.0
 	github.com/knadh/koanf/providers/file v1.2.0
-	github.com/knadh/koanf/v2 v2.2.1
+	github.com/knadh/koanf/v2 v2.2.2
 	github.com/mattn/go-sqlite3 v1.14.28
 	github.com/prometheus/client_golang v1.22.0
 	github.com/redis/go-redis/v9 v9.11.0
 	github.com/rs/zerolog v1.34.0
+	go.opentelemetry.io/otel v1.37.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.37.0
+	go.opentelemetry.io/otel/sdk v1.37.0
+	go.opentelemetry.io/otel/trace v1.37.0
 	modernc.org/sqlite v1.38.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
-	github.com/bytedance/sonic/loader v0.2.4 // indirect
+	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cenkalti/backoff/v5 v5.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -29,12 +34,15 @@ require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.3.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
 	github.com/knadh/koanf/maps v0.1.2 // indirect
@@ -51,20 +59,27 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.65.0 // indirect
-	github.com/prometheus/procfs v0.16.1 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.7.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/arch v0.18.0 // indirect
-	golang.org/x/crypto v0.39.0 // indirect
-	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/arch v0.19.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/exp v0.0.0-20250711185948-6ae5c78190dc // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/grpc v1.73.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	modernc.org/libc v1.66.2 // indirect
+	modernc.org/libc v1.66.3 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
 )
---
+++ tracing/tracing.go
@@ -0,0 +1,156 @@
+package tracing
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/meysam81/x/logging"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type TracingConfig struct {
+	ServiceName     string
+	ServiceVersion  string
+	OTLPEndpointURL string
+	Enabled         bool
+	ServerName      string
+}
+
+type Tracer struct {
+	provider *sdktrace.TracerProvider
+	tracer   trace.Tracer
+	logger   *logging.Logger
+	config   *TracingConfig
+}
+
+func NewTracer(ctx context.Context, config *TracingConfig, logger *logging.Logger) (*Tracer, error) {
+	if !config.Enabled {
+		return &Tracer{logger: logger}, nil
+	}
+
+	exporter, err := otlptracehttp.New(ctx,
+		otlptracehttp.WithEndpointURL(config.OTLPEndpointURL),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	hostname, _ := os.Hostname()
+	if hostname == "" {
+		hostname = "unknown"
+	}
+
+	serverName := config.ServerName
+	if serverName == "" {
+		serverName = hostname
+	}
+
+	resource, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(config.ServiceName),
+			semconv.ServiceVersion(config.ServiceVersion),
+			semconv.ServerAddress(serverName),
+			semconv.HostName(hostname),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	provider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource),
+	)
+
+	otel.SetTracerProvider(provider)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	tracer := provider.Tracer(config.ServiceName)
+
+	return &Tracer{
+		provider: provider,
+		tracer:   tracer,
+		logger:   logger,
+		config:   config,
+	}, nil
+}
+
+func (t *Tracer) GetTracer() trace.Tracer {
+	if t.tracer == nil {
+		return otel.GetTracerProvider().Tracer("noop")
+	}
+	return t.tracer
+}
+
+func (t *Tracer) Shutdown(ctx context.Context) error {
+	if t.provider == nil {
+		return nil
+	}
+	return t.provider.Shutdown(ctx)
+}
+
+func (t *Tracer) StartSpan(ctx context.Context, spanName string) (context.Context, trace.Span) {
+	if t.tracer == nil {
+		return ctx, trace.SpanFromContext(ctx)
+	}
+	return t.tracer.Start(ctx, spanName)
+}
+
+func (t *Tracer) DetachSpanFromContext(ctx context.Context) context.Context {
+	span := trace.SpanFromContext(ctx)
+	if !span.SpanContext().IsValid() {
+		return context.Background()
+	}
+	return trace.ContextWithSpan(context.Background(), span)
+}
+
+func (t *Tracer) HTTPMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if t.tracer == nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		ctx := otel.GetTextMapPropagator().Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+		ctx, span := t.tracer.Start(ctx, r.Method+" "+r.URL.Path, trace.WithSpanKind(trace.SpanKindServer))
+		defer span.End()
+
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		defer func() {
+			span.SetAttributes(
+				semconv.HTTPResponseStatusCode(ww.Status()),
+			)
+		}()
+
+		span.SetAttributes(
+			semconv.ServiceName(t.config.ServiceName),
+			semconv.HTTPRequestMethodKey.String(r.Method),
+			semconv.HTTPRoute(r.URL.Path),
+			semconv.URLPath(r.URL.Path),
+			semconv.URLQuery(r.URL.RawQuery),
+			semconv.URLScheme(r.URL.Scheme),
+			semconv.ServerAddress(r.Host),
+			semconv.UserAgentOriginal(r.UserAgent()),
+			semconv.NetworkProtocolName("http"),
+			semconv.NetworkProtocolVersion(r.Proto),
+		)
+
+		r = r.WithContext(ctx)
+		next.ServeHTTP(ww, r)
+	})
+}
